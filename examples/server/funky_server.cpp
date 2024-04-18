658edd6f010fdd37e2ab27a58e91c1dfc3e49332
Michael Niederle <mniederle@gmx.at>
2024-04-18 11:35:34 +0200

    Removed more Windows incompatible features

--- examples/server/funky_server.cpp
+++ examples/server/funky_server.cpp
@@ -1786,19 +1786,19 @@ int main(int argc, char **argv)
     }
 
     if (do_test_hardware) {
-	__builtin_cpu_init();
-	bool has_avx = __builtin_cpu_supports("avx");
-	bool has_avx2 = __builtin_cpu_supports("avx2");
-	bool has_avx512 = __builtin_cpu_supports("avx512f");
-	printf(
-	    "avx:  %s\n"
-	    "avx2: %s\n"
-	    "avx512: %s\n",
-	    has_avx ? "yes" : "no",
-	    has_avx2 ? "yes" : "no",
-	    has_avx512 ? "yes" : "no"
-	);
 	#ifndef _WIN32
+	  __builtin_cpu_init();
+	  bool has_avx = __builtin_cpu_supports("avx");
+	  bool has_avx2 = __builtin_cpu_supports("avx2");
+	  bool has_avx512 = __builtin_cpu_supports("avx512f");
+	  printf(
+	      "avx:  %s\n"
+	      "avx2: %s\n"
+	      "avx512: %s\n",
+	      has_avx ? "yes" : "no",
+	      has_avx2 ? "yes" : "no",
+	      has_avx512 ? "yes" : "no"
+	  );
 	  struct sysinfo info;
 	  if (sysinfo(&info) == 0) {
 	    printf("ram: %lu\n", info.totalram);
